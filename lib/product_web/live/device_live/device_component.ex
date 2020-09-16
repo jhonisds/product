cdd2ab560c806b329efd2f0ff469547e203d8977
Jhoni Santos <jhoni.sousa@fatec.sp.gov.br>
2020-09-15 23:36:01 -0300

    create handle event duplicate.

--- lib/product_web/live/device_live/device_component.ex
+++ lib/product_web/live/device_live/device_component.ex
@@ -1,11 +1,38 @@
 defmodule ProductWeb.DeviceLive.DeviceComponent do
   use ProductWeb, :live_component
 
+  alias Product.Devices
+
   def render(assigns) do
-    IO.inspect(assigns)
+    IO.inspect(assigns, label: "DEVICE COMPONENT")
 
     ~L"""
-        Device Component
+        <div class="row">
+          <div class="column column-10">
+            <%= @device.id %>
+          </div>
+          <div class="column column-10">
+            <%= @device.name %>
+          </div>
+          <div class="column column-10">
+            <%= @device.sku %>
+          </div>
+          <div class="column column-10">
+            <%= @device.price %>
+          </div>
+          <div class="column column-10">
+            <a href="#" phx-click="duplicate" phx-target="<%= @myself %>">
+             add
+            </a>
+          </div>
+        </div>
     """
+  end
+
+  def handle_event("duplicate", _, socket) do
+    device = socket.assigns.device
+    insert = %{name: device.name, sku: device.sku, price: device.price}
+    Devices.create_device(insert)
+    {:noreply, socket}
   end
 end
