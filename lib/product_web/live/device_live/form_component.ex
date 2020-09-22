5643df140302d539a4442439303b451a882a3a3b
Jhoni Santos <jhoni.sousa@fatec.sp.gov.br>
2020-09-21 23:51:23 -0300

    building modal form with component.

--- lib/product_web/live/device_live/form_component.ex
+++ lib/product_web/live/device_live/form_component.ex
@@ -1,11 +1,29 @@
 defmodule ProductWeb.DeviceLive.FormComponent do
   use ProductWeb, :live_component
 
-  def render(assigns) do
-    IO.inspect("FORM COMPONENT")
+  alias Product.Devices
 
-    ~L"""
-      FORM COMPONENT
-    """
+  @impl true
+  def update(%{device: device} = assigns, socket) do
+    changeset = Devices.change_device(device)
+
+    {:ok,
+     socket
+     |> assign(assigns)
+     |> assign(:changeset, changeset)}
+  end
+
+  @impl true
+  def handle_event("validate", %{"device" => device}, socket) do
+    changeset =
+      socket.assigns.device
+      |> Devices.change_device(device)
+      |> Map.put(:action, :validate)
+
+    {:noreply, assign(socket.assigns.action, changeset)}
+  end
+
+  @impl true
+  def handle_event("save", %{"device" => device}, socket) do
   end
 end
